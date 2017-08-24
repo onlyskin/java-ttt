652276495d8225dde2a5838a2755031e709f719c
Sam Szreter <s.szreter@gmail.com>
2017-08-24 17:22:10 +0100

    Add tied game integration test

--- src/test/java/ttt/GameTest.java
+++ src/test/java/ttt/GameTest.java
@@ -27,6 +27,23 @@ public class GameTest {
                 "X won\n", out.toString());
     }
 
+    @Test
+    public void RunsTiedGame() throws Exception {
+        runGameWithUserInput("0\n1\n6\n3\n7\n8\n5\n2\n4\nexit\n");
+        assertEquals("start\n" +
+                "---\n---\n---\n\n" +
+                "X--\n---\n---\n\n" +
+                "XO-\n---\n---\n\n" +
+                "XO-\n---\nX--\n\n" +
+                "XO-\nO--\nX--\n\n" +
+                "XO-\nO--\nXX-\n\n" +
+                "XO-\nO--\nXXO\n\n" +
+                "XO-\nO-X\nXXO\n\n" +
+                "XOO\nO-X\nXXO\n\n" +
+                "XOO\nOXX\nXXO\n\n" +
+                "a tie\n", out.toString());
+    }
+
     private void runGameWithUserInput(String userInput) throws Exception {
         InputStream inputStream = new ByteArrayInputStream(userInput.getBytes());
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
