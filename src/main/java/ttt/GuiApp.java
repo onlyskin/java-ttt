faa1af3e8e3002975e7a7ccb73bcc6124b276857
Sam Szreter <s.szreter@gmail.com>
2017-09-12 17:32:58 +0100

    Make board resize on window change

--- src/main/java/ttt/GuiApp.java
+++ src/main/java/ttt/GuiApp.java
@@ -1,6 +1,8 @@
 package ttt;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,33 +13,39 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class GuiApp extends Application {
-    private final Integer windowSize = 600;
+    private final double windowSize = 600;
+    private final double cellSize = windowSize / 3;
+    private StackPane[] tiles = new StackPane[9];
+    private Rectangle[] rectangles = new Rectangle[9];
 
     private Parent createContent() {
         Pane pane = new Pane();
         pane.setPrefSize(windowSize+1, windowSize+1);
 
-        for (int i=0;i<3;i++) {
-            for (int j=0;j<3;j++) {
-                Tile tile = new Tile();
-                tile.setTranslateX(j * windowSize / 3);
-                tile.setTranslateY(i * windowSize / 3);
-
-                pane.getChildren().add(tile);
-            }
+        for (int i=0;i<9;i++) {
+            Rectangle rectangle = new Rectangle();
+            StackPane stackPane = new StackPane();
+            rectangles[i] = rectangle;
+            tiles[i] = stackPane;
+            
+            rectangle.setFill(null);
+            rectangle.setStroke(Color.BLACK);
+            stackPane.setAlignment(Pos.CENTER);
+            stackPane.getChildren().addAll(rectangle);
+            pane.getChildren().add(stackPane);
         }
+
+        updateContent(cellSize);
 
         return pane;
     }
 
-    private class Tile extends StackPane {
-        public Tile () {
-            Rectangle rectangle = new Rectangle(windowSize / 3, windowSize / 3);
-            rectangle.setFill(null);
-            rectangle.setStroke(Color.BLACK);
-
-            setAlignment(Pos.CENTER);
-            getChildren().addAll(rectangle);
+    private void updateContent(double cellSize) {
+        for (int i=0;i<9;i++) {
+            rectangles[i].setHeight(cellSize);
+            rectangles[i].setWidth(cellSize);
+            tiles[i].setTranslateX((i%3) * cellSize);
+            tiles[i].setTranslateY((i/3) * cellSize);
         }
     }
 
@@ -48,6 +56,11 @@ public class GuiApp extends Application {
         primaryStage.show();
         primaryStage.minWidthProperty().bind(scene.heightProperty().multiply(1));
         primaryStage.minHeightProperty().bind(scene.widthProperty().multiply(1));
+        primaryStage.minHeightProperty().addListener(new ChangeListener() {
+            @Override public void changed(ObservableValue o, Object oldValue, Object newValue) {
+                updateContent(primaryStage.getMinHeight() / 3);
+            }
+        });
     }
 
     public static void main(String[] args) {
