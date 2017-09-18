5fe00e27be7a6009f1a0a83f9bd4d4f055f78a15
Sam Szreter <s.szreter@gmail.com>
2017-09-18 15:57:09 +0100

    Update to use FXML individual elements with a GridPane and some styling

--- src/main/java/ttt/FxFXMLController.java
+++ src/main/java/ttt/FxFXMLController.java
@@ -1,31 +1,101 @@
 package ttt;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
-public class FxFXMLController 
+public class FxFXMLController
 {
     @FXML
-    private Text text;
+    private Text text1;
+
+    @FXML
+    private Text text2;
+
+    @FXML
+    private Text text3;
+
+    @FXML
+    private Text text4;
+
+    @FXML
+    private Text text5;
+
+    @FXML
+    private Text text6;
+
+    @FXML
+    private Text text7;
+
+    @FXML
+    private Text text8;
+
+	@FXML
+	private Text text9;
 
 	@FXML
 	private URL location;
 
 	@FXML
 	private ResourceBundle resources;
-	
-	public FxFXMLController() 
+
+    @FXML
+    private Rectangle rect1;
+
+    @FXML
+    private Rectangle rect2;
+
+    @FXML
+    private Rectangle rect3;
+
+    @FXML
+    private Rectangle rect4;
+
+    @FXML
+    private Rectangle rect5;
+
+    @FXML
+    private Rectangle rect6;
+
+    @FXML
+    private Rectangle rect7;
+
+    @FXML
+    private Rectangle rect8;
+
+	@FXML
+    private Rectangle rect9;
+
+    private Random random;
+
+	public FxFXMLController()
 	{
+	    this.random = new Random();
 	}
-	
+
+	private String getRandomMarker() {
+	    int value = random.nextInt(2);
+	    if (value == 0) {
+	        return "X";
+        } else {
+	        return "O";
+        }
+    }
+
 	@FXML
 	private void initialize() {
-	    text.setText("test");
+	    text1.setText(getRandomMarker());
+        text2.setText(getRandomMarker());
+        text3.setText(getRandomMarker());
+        text4.setText(getRandomMarker());
+        text5.setText(getRandomMarker());
+        text6.setText(getRandomMarker());
+        text7.setText(getRandomMarker());
+        text8.setText(getRandomMarker());
+        text9.setText(getRandomMarker());
 	}
 }
--- src/main/java/ttt/GuiApp.java
+++ src/main/java/ttt/GuiApp.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GuiApp extends Application
@@ -19,7 +19,7 @@ public class GuiApp extends Application
     @Override
     public void start(Stage stage) throws IOException
     {
-        VBox root = (VBox) FXMLLoader.load(getClass().getResource("GuiApp.fxml"));
+        GridPane root = (GridPane) FXMLLoader.load(getClass().getResource("GuiApp.fxml"));
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
